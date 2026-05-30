588dd1ed487b415277276f2e80a5b5f144e57bd3
asyncbuild <contact.deepeshreddy@gmail.com>
2026-05-31 00:12:55 +0530

    usePrev concept

--- Week-10/customHooks-1/src/App.jsx
+++ Week-10/customHooks-1/src/App.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react"
 import { useFetch } from "./hooks/useFetch"
+import { usePrev } from "./hooks/usePrev";
  
 function App() {
   const [currentPost,setCurrentPost] = useState(1);
   const {finalData,loading} = useFetch(`https://jsonplaceholder.typicode.com/posts/${currentPost}`);
+  const [count,setCount] = useState(0);
+  const prev = usePrev(count)
   if(loading){
     return <div>Loading......</div>
   }
@@ -16,6 +19,10 @@ function App() {
       <button onClick={()=>setCurrentPost(3)} >3</button>
       <br />
       {finalData.title}
+      <br />
+      <button onClick={()=>setCount(count+1)} >+</button>
+      <h1>Current Count: {count}</h1>
+      <h1>Previous Count: {prev}</h1>
     </div>
   )
 }
