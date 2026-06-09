0f87d428f93d57356f6f63d8230451a97e1cfd43
asyncbuild <contact.deepeshreddy@gmail.com>
2026-06-09 09:57:42 +0530

    typescript basics

--- Week-13/project/tailwindProject/src/components/1-basic-project.jsx
+++ Week-13/project/tailwindProject/src/components/1-basic-project.jsx
@@ -1,7 +1,7 @@
 function SidebarClass(){
     return (
         <div className="flex">
-            <div className="bg-pink-300 h-screen transition-all delay-1000 p-2 md:w-96 w-0 ">
+            <div className="bg-pink-300 h-screen transition-all duration-2000 p-2 md:w-96 w-0 ">
                 Side Bar
             </div>
             <div className="bg-gray-400 w-full h-screen p-2">
---
+++ Week-14/typescript1/typescript.ts
@@ -0,0 +1,20 @@
+function prob1(firstName : string){
+    console.log(`Hello ${firstName}`);
+    
+}
+
+//prob1("Deepesh")
+
+function prob2(num1 : number, num2 : number) : number{
+    return num1 + num2;
+}
+
+// console.log(prob2(5, 10));
+
+function delayedCall(fn : () => void){
+    setTimeout(fn, 2000);
+}
+
+delayedCall(function(){
+    console.log("This is a delayed call");
+})
