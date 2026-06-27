2a57f3bfc7fd1035e5d5954f75b058278cea8d58
asyncbuild <contact.deepeshreddy@gmail.com>
2026-06-27 11:44:30 +0530

    typescript syntax

---
+++ Week-14/typescript2/typescript2/src/App.tsx
@@ -0,0 +1,33 @@
+
+function operation(){
+  interface User {
+      name: string;
+      age: number;
+    }
+
+    function sumOfAge(user1: User, user2: User) {
+      return user1.age + user2.age;
+    };
+
+    // Example usage
+    const result = sumOfAge({
+      name: "harkirat",
+      age: 20
+    }, {
+      name: "raman",
+      age: 21
+    });
+    console.log(result); // Output: 9
+    return result;
+}
+
+function App() {
+    
+  return (
+    <div>
+      {operation()}
+    </div>
+  )
+}
+
+export default App
